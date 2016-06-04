3973d2149e988296e159ce2c50df8c650f9307ba
ses10 <sesma@live.com>
2016-06-04 14:58:29 -0400

    solved delete node in a linked list

---
+++ LeetCode/delete-node-linked-list.js
@@ -0,0 +1,26 @@
+#
+#Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+#
+#Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+#
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """ 
+        #to delete a node in chain without reference to previous node
+        #copy value of next node to current node and set current node's next
+        #to the node after the next node
+       
+        node.val = node.next.val
+        temp = node.next
+        node.next = node.next.next
+        temp = None
