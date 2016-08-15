09d772639761721d84850661ce4abb266ea335ee
ses10 <sesma@live.com>
2016-08-15 09:17:12 -0400

    improved from O(2n) to 0(n)

--- LeetCode/merge-two-sorted-lists.java
+++ LeetCode/merge-two-sorted-lists.java
@@ -8,81 +8,43 @@
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
-        //our new merged list
-        ListNode head = null;
+    
+        ListNode dummy = new ListNode(0);
         ListNode p1 = l1;
         ListNode p2 = l2;
+        //our new merged list
+        ListNode head = dummy;
         
-        //splice together both lists
         while(p1 != null || p2 != null)
         {
+            ListNode cur;
+            
             if(p1 != null && p2 == null)
             {
-                head = addN(head, p1.val);
+                cur = p1;
                 p1 = p1.next;
             }
             else if(p2 != null && p1 == null)
             {
-                head = addN(head, p2.val);
+                cur = p2;
                 p2 = p2.next;
             }
             else if(p1.val < p2.val)
             {
-                head = addN(head, p1.val);
+                cur = p1;
                 p1 = p1.next;
             }
             else
             {
-                head = addN(head, p2.val);
+                cur = p2;
                 p2 = p2.next;
             }
-        }
-
-        //because of the way linked list insert nodes we need to reverse it
-        head = revList(head);
-        
-        return head;
-        
-    }
-    
-    public ListNode addN(ListNode head, int val)
-    {
-        ListNode node = new ListNode(val);
-        
-        if(head == null)
-            head = node;
-        else
-        {
-            node.next = head;
-            head = node;
-        }
-        
-        return head;
-        
-    }
-    
-    public ListNode revList(ListNode head)
-    {
-        if(head == null)
-            return head;
-        else if(head.next == null)
-            return head;
-        
-        ListNode prev = null;
-        ListNode mid = head;
-        ListNode next = head.next;
-        
-        while(true)
-        {
-            mid.next = prev;
-            prev = mid;
-            mid = next;
-            if(mid == null)
-                break;
-            next = next.next;
             
+            dummy.next = cur;
+            dummy = dummy.next;
         }
-        return prev;
+        
+        return head.next;
+        
     }
 }
